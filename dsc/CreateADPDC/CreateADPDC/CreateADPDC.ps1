7834c09acb5357006ff92cb1eb9962599428b16c
Kasi Srinivasan <kasi.srinivasan@unisys.com>
2019-09-16 17:27:20 -0400

    Restored the Powershell DSC with AD components

--- dsc/CreateADPDC/CreateADPDC/CreateADPDC.ps1
+++ dsc/CreateADPDC/CreateADPDC/CreateADPDC.ps1
@@ -20,89 +20,89 @@
 
     Node localhost
     {
-        # LocalConfigurationManager 
-        # {
-        #     RebootNodeIfNeeded = $true
-        # }
+        LocalConfigurationManager 
+        {
+            RebootNodeIfNeeded = $true
+        }
 
-	    # WindowsFeature DNS 
-        # { 
-        #     Ensure = "Present" 
-        #     Name = "DNS"		
-        # }
+	    WindowsFeature DNS 
+        { 
+            Ensure = "Present" 
+            Name = "DNS"		
+        }
 
-        # Script EnableDNSDiags
-	    # {
-      	#     SetScript = { 
-		#         Set-DnsServerDiagnostics -All $true
-        #         Write-Verbose -Verbose "Enabling DNS client diagnostics" 
-        #     }
-        #     GetScript =  { @{} }
-        #     TestScript = { $false }
-	    #     DependsOn = "[WindowsFeature]DNS"
-        # }
+        Script EnableDNSDiags
+	    {
+      	    SetScript = { 
+		        Set-DnsServerDiagnostics -All $true
+                Write-Verbose -Verbose "Enabling DNS client diagnostics" 
+            }
+            GetScript =  { @{} }
+            TestScript = { $false }
+	        DependsOn = "[WindowsFeature]DNS"
+        }
 
-	    # WindowsFeature DnsTools
-	    # {
-	    #     Ensure = "Present"
-        #     Name = "RSAT-DNS-Server"
-        #     DependsOn = "[WindowsFeature]DNS"
-	    # }
+	    WindowsFeature DnsTools
+	    {
+	        Ensure = "Present"
+            Name = "RSAT-DNS-Server"
+            DependsOn = "[WindowsFeature]DNS"
+	    }
 
-        # xDnsServerAddress DnsServerAddress 
-        # { 
-        #     Address        = '127.0.0.1' 
-        #     InterfaceAlias = $InterfaceAlias
-        #     AddressFamily  = 'IPv4'
-	    #     DependsOn = "[WindowsFeature]DNS"
-        # }
+        xDnsServerAddress DnsServerAddress 
+        { 
+            Address        = '127.0.0.1' 
+            InterfaceAlias = $InterfaceAlias
+            AddressFamily  = 'IPv4'
+	        DependsOn = "[WindowsFeature]DNS"
+        }
 
-        # xWaitforDisk Disk2
-        # {
-        #     DiskNumber = 2
-        #     RetryIntervalSec =$RetryIntervalSec
-        #     RetryCount = $RetryCount
-        # }
+        xWaitforDisk Disk2
+        {
+            DiskNumber = 2
+            RetryIntervalSec =$RetryIntervalSec
+            RetryCount = $RetryCount
+        }
 
-        # xDisk ADDataDisk {
-        #     DiskNumber = 2
-        #     DriveLetter = "F"
-        #     DependsOn = "[xWaitForDisk]Disk2"
-        # }
+        xDisk ADDataDisk {
+            DiskNumber = 2
+            DriveLetter = "F"
+            DependsOn = "[xWaitForDisk]Disk2"
+        }
 
-        # WindowsFeature ADDSInstall 
-        # { 
-        #     Ensure = "Present" 
-        #     Name = "AD-Domain-Services"
-	    #     DependsOn="[WindowsFeature]DNS" 
-        # } 
+        WindowsFeature ADDSInstall 
+        { 
+            Ensure = "Present" 
+            Name = "AD-Domain-Services"
+	        DependsOn="[WindowsFeature]DNS" 
+        } 
 
-        # WindowsFeature ADDSTools
-        # {
-        #     Ensure = "Present"
-        #     Name = "RSAT-ADDS-Tools"
-        #     DependsOn = "[WindowsFeature]ADDSInstall"
-        # }
+        WindowsFeature ADDSTools
+        {
+            Ensure = "Present"
+            Name = "RSAT-ADDS-Tools"
+            DependsOn = "[WindowsFeature]ADDSInstall"
+        }
 
-        # WindowsFeature ADAdminCenter
-        # {
-        #     Ensure = "Present"
-        #     Name = "RSAT-AD-AdminCenter"
-        #     DependsOn = "[WindowsFeature]ADDSInstall"
-        # }
+        WindowsFeature ADAdminCenter
+        {
+            Ensure = "Present"
+            Name = "RSAT-AD-AdminCenter"
+            DependsOn = "[WindowsFeature]ADDSInstall"
+        }
          
-        # xADDomain FirstDS 
-        # {
-        #     DomainName = $DomainName
-        #     DomainAdministratorCredential = $DomainCreds
-        #     SafemodeAdministratorPassword = $DomainCreds
-        #     DatabasePath = "F:\NTDS"
-        #     LogPath = "F:\NTDS"
-        #     SysvolPath = "F:\SYSVOL"
-	    #     DependsOn = @("[xDisk]ADDataDisk", "[WindowsFeature]ADDSInstall")
-        # } 
+        xADDomain FirstDS 
+        {
+            DomainName = $DomainName
+            DomainAdministratorCredential = $DomainCreds
+            SafemodeAdministratorPassword = $DomainCreds
+            DatabasePath = "F:\NTDS"
+            LogPath = "F:\NTDS"
+            SysvolPath = "F:\SYSVOL"
+	        DependsOn = @("[xDisk]ADDataDisk", "[WindowsFeature]ADDSInstall")
+        } 
 
-            #Install the IIS Role
+        #Install the IIS Role
         WindowsFeature IIS
         {
             Ensure = "Present"

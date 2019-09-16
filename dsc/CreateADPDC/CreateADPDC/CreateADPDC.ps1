b3d2b39ab54fffaa4ea07f8c294f12b2c339c681
Kasi Srinivasan <kasi.srinivasan@unisys.com>
2019-09-15 23:06:06 -0400

    commented out the additional windows features

---
+++ dsc/CreateADPDC/CreateADPDC/CreateADPDC.ps1
@@ -0,0 +1,151 @@
+﻿configuration CreateADPDC 
+{ 
+   param 
+   ( 
+        [Parameter(Mandatory)]
+        [String]$DomainName,
+
+        [Parameter(Mandatory)]
+        [System.Management.Automation.PSCredential]$Admincreds,
+
+        [Int]$RetryCount=20,
+        [Int]$RetryIntervalSec=30
+    ) 
+    
+    Import-DscResource -ModuleName xActiveDirectory, xStorage, xNetworking, PSDesiredStateConfiguration, xPendingReboot
+    [System.Management.Automation.PSCredential ]$DomainCreds = New-Object System.Management.Automation.PSCredential ("${DomainName}\$($Admincreds.UserName)", $Admincreds.Password)
+    $Interface=Get-NetAdapter|Where Name -Like "Ethernet*"|Select-Object -First 1
+    $InterfaceAlias=$($Interface.Name)
+
+    Node localhost
+    {
+        LocalConfigurationManager 
+        {
+            RebootNodeIfNeeded = $true
+        }
+
+	    WindowsFeature DNS 
+        { 
+            Ensure = "Present" 
+            Name = "DNS"		
+        }
+
+        Script EnableDNSDiags
+	    {
+      	    SetScript = { 
+		        Set-DnsServerDiagnostics -All $true
+                Write-Verbose -Verbose "Enabling DNS client diagnostics" 
+            }
+            GetScript =  { @{} }
+            TestScript = { $false }
+	        DependsOn = "[WindowsFeature]DNS"
+        }
+
+	    WindowsFeature DnsTools
+	    {
+	        Ensure = "Present"
+            Name = "RSAT-DNS-Server"
+            DependsOn = "[WindowsFeature]DNS"
+	    }
+
+        xDnsServerAddress DnsServerAddress 
+        { 
+            Address        = '127.0.0.1' 
+            InterfaceAlias = $InterfaceAlias
+            AddressFamily  = 'IPv4'
+	        DependsOn = "[WindowsFeature]DNS"
+        }
+
+        xWaitforDisk Disk2
+        {
+            DiskNumber = 2
+            RetryIntervalSec =$RetryIntervalSec
+            RetryCount = $RetryCount
+        }
+
+        xDisk ADDataDisk {
+            DiskNumber = 2
+            DriveLetter = "F"
+            DependsOn = "[xWaitForDisk]Disk2"
+        }
+
+        WindowsFeature ADDSInstall 
+        { 
+            Ensure = "Present" 
+            Name = "AD-Domain-Services"
+	        DependsOn="[WindowsFeature]DNS" 
+        } 
+
+        WindowsFeature ADDSTools
+        {
+            Ensure = "Present"
+            Name = "RSAT-ADDS-Tools"
+            DependsOn = "[WindowsFeature]ADDSInstall"
+        }
+
+        WindowsFeature ADAdminCenter
+        {
+            Ensure = "Present"
+            Name = "RSAT-AD-AdminCenter"
+            DependsOn = "[WindowsFeature]ADDSInstall"
+        }
+         
+        xADDomain FirstDS 
+        {
+            DomainName = $DomainName
+            DomainAdministratorCredential = $DomainCreds
+            SafemodeAdministratorPassword = $DomainCreds
+            DatabasePath = "F:\NTDS"
+            LogPath = "F:\NTDS"
+            SysvolPath = "F:\SYSVOL"
+	        DependsOn = @("[xDisk]ADDataDisk", "[WindowsFeature]ADDSInstall")
+        } 
+
+    #         #Install the IIS Role
+    #     WindowsFeature IIS
+    #     {
+    #     Ensure = "Present"
+    #     Name = "Web-Server"
+    #     }
+
+    #     #Install ASP.NET 4.5
+    #     WindowsFeature ASP
+    #     {
+    #     Ensure = "Present"
+    #     Name = "Web-Asp-Net45"
+    #     }
+
+    # #Install Feature "HTTP Activation"
+    #     WindowsFeature NET-WCF-HTTP-Activation45
+    #     {
+    #         Ensure = "Present"
+    #         Name = "NET-WCF-HTTP-Activation45"
+    #     }
+
+    #     #Install RoleService 'Message Queuing Activation'
+    #     WindowsFeature NET-WCF-MSMQ-Activation45
+    #     {
+    #         Ensure = "Present"
+    #         Name = "NET-WCF-MSMQ-Activation45"
+    #     }
+
+    #     #Install Feature "Message Queuing"
+    #         WindowsFeature MSMQ
+    #         {
+    #             Ensure = "Present"
+    #             Name = "MSMQ"
+    #         }
+    #     #Install Feature "Message Queuing Services"
+    #         WindowsFeature MSMQ-Services
+    #         {
+    #         Ensure = "Present"
+    #         Name = "MSMQ-Services"
+    #         }
+    #         #Install Feature "Message Queuing Server"
+    #             WindowsFeature MSMQ-Server
+    #             {
+    #                 Ensure = "Present"
+    #                 Name = "MSMQ-Server"
+    #             }
+     }
+} 
